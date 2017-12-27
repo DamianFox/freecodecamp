510877fff1684e57bc8b7a61de554df051634cdf
damiano.fossa <d.fossa.91@gmail.com>
2017-12-27 23:22:38 +0100

    Added function which changes the icon according to the forecast

--- local-weather/js/weather.js
+++ local-weather/js/weather.js
@@ -12,11 +12,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	tempEl = document.getElementById("temp");
 
 	// Get the temperature unit element
-	tempUnitEl = document.getElementById("tempUnit");
+	tempUnitEl = document.getElementById("temp-unit");
 
+	// Add the click event to the temperature unit element
 	tempUnitEl.addEventListener("click", changeUnit);
 
+	// Weather forecast API url
 	weatherAPI = "https://fcc-weather-api.glitch.me/api/current";
+
+	// Get the description element
+	weatherIconEl = document.getElementById("weather-icon");
 
 	// Initialize Geocoder
 	geocoder = new google.maps.Geocoder();
@@ -25,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	getCurrentLocation();
 });
 
+// Get current location coordinates 
 function getCurrentLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getPosition, geoError);
@@ -41,10 +47,13 @@ function getPosition(position) {
     getAddressFromCoords(latitude, longitude);
 }
 
+// Callback function in case of geolocation error
 function geoError() {
     console.log("Geocoder failed.");
 }
 
+// Get the right address from latitude and longitude
+// Finally,  print it in the right tag element
 function getAddressFromCoords(lat, lng) {
     var latlng = new google.maps.LatLng(lat, lng);
     geocoder.geocode({'latLng': latlng}, function(results, status) {
@@ -58,13 +67,14 @@ function getAddressFromCoords(lat, lng) {
 						if (err !== null) {
 							console.log('Something went wrong: ' + err);
 						} else {
-							// console.log('Data: ' + data.weather[0].description);
-							description = data.weather[0].description;
-							descEl.innerHTML = description;
+							// description = data.weather[0].description;
+							// descEl.innerHTML = description;
 
 							temp = data.main.temp;
 							tempEl.innerHTML = temp;
 							tempUnitEl.innerHTML = " °C";
+
+							iconGen(data.weather[0].main);
 						}
 					}
 				);
@@ -77,23 +87,53 @@ function getAddressFromCoords(lat, lng) {
     });
 }
 
+// Change temparature unit and value 
 function changeUnit(){
 	var newTempEl = document.getElementById("temp").innerHTML;
-	var currentTempUnit = document.getElementById("tempUnit").innerHTML;
+	var currentTempUnit = document.getElementById("temp-unit").innerHTML;
 	var newTempUnit = currentTempUnit == " °C" ? " °F" : " °C";
 	var newTempElValue = currentTempUnit == " °C" ? toFahrenheit(newTempEl) : toCelsius(newTempEl);
 	tempEl.innerHTML = newTempElValue;
 	tempUnit.innerHTML = newTempUnit;
 }
 
+// Celsius to Fahrenheit
 function toFahrenheit(celsius){
 	return (celsius * 1.8) + 32;
 }
 
+// Fahrenheit to Celsius
 function toCelsius(fahrenheit){
 	return (fahrenheit - 32) / 1.8;
 }
 
+function iconGen(desc) {
+	var desc = desc.toLowerCase()
+	switch (desc) {
+		case 'drizzle':
+      		weatherIconEl.className += " wi-rain";
+      		break;
+    	case 'clouds':
+ 	 		weatherIconEl.className += " wi-cloudy";
+      		break;
+    	case 'rain':
+      		weatherIconEl.className += " wi-rain";
+      		break;
+		case 'snow':
+      		weatherIconEl.className += " wi-snow";
+      		break;
+		case 'clear':
+      		weatherIconEl.className += " wi-day-sunny";
+      		break;
+		case 'thunderstom':
+      		weatherIconEl.className += " wi-thunderstorm";
+      		break;
+	    default:
+      		weatherIconEl.className = "wi";
+  	}
+}
+
+// Get JSON value from url
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
