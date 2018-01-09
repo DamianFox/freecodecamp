4ac1bdaef184894d3559f09d8d4923eccf0d2cc6
damiano.fossa <d.fossa.91@gmail.com>
2018-01-09 21:37:44 +0100

    webpack config updated

--- wikipedia-viewer/webpack.config.js
+++ wikipedia-viewer/webpack.config.js
@@ -1,16 +1,20 @@
 const webpack = require('webpack');
 const path = require('path');
 
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'src');
+
 const config = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: BUILD_DIR,
         filename: 'bundle.js'
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
+                include : APP_DIR,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             }
