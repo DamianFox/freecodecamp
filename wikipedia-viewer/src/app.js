05d6381ba4d74af111aef8ebe96dacf8ec6d141c
damiano.fossa <d.fossa.91@gmail.com>
2018-01-22 23:23:51 +0100

    bug fixes

--- wikipedia-viewer/src/app.js
+++ wikipedia-viewer/src/app.js
@@ -3,29 +3,29 @@ import ReactDOM from "react-dom";
 // import axios from 'axios';
 
 class Homepage extends Component {
-	constructor() {
-		super();
-		this.state = {
-			search: ''
-		}
-		this.getData = this.getData.bind(this);
-	}
+    constructor() {
+        super();
+        this.state = {
+            search: ''
+        }
+        this.getData = this.getData.bind(this);
+    }
 
-  	getData(e) {
+    getData(e) {
         e.preventDefault();
         var search = e.target.search.value;
         var wikipediaEndPoint = "https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&generator=search&gsrnamespace=0&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="+search;
         var wikipediaUrl = "https://en.wikipedia.org/?curid=";
 
-    	fetch(wikipediaEndPoint)
-    	.then(data => {
-    		return data.json();
-    	}).then(data => {
+        fetch(wikipediaEndPoint)
+        .then(data => {
+            return data.json();
+        }).then(data => {
             var objectList = (data.hasOwnProperty("query")) ? data.query.pages : "";
-            let resultRender;
+            var resultRender;
 
-            if(objectList !== ""){
-                const ResultList = Object.keys(objectList).map(function(key, index) {
+            if(objectList !== "") {
+                resultRender = Object.keys(objectList).map(function(key, index) {
                     return (
                         <div className="item" key={objectList[key].pageid}>
                             <a href={wikipediaUrl+objectList[key].pageid}>
@@ -35,35 +35,35 @@ class Homepage extends Component {
                         </div>
                     )
                 });
-                resultRender = (<ResultList />)
             } else {
-                const NoResult = (function() {
-                    return (
+                resultRender = (
                         <div className="item" key={1}>
                             <p>No results!</p>
                         </div>
                     )
-                });
-                resultRender = (<NoResult />);
             }
 
-    		this.setState({search: resultRender});
-    	})
+            this.setState({search: resultRender});
+        })
+        .catch(error => {
+            console.error(error);
+        });
     }
 
-	render() {
-	    return (
-			<div className="container">
-	    		<h1>Wikipedia Viewer</h1>
-                <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">Random article</a>
-	    		<form onSubmit={this.getData}>
-	    			<input type="text" id="search" name="search" placeholder="Search Wikipedia" />
-	    			<button>Click</button>
-	    		</form>
-	    		<div className="items">{this.state.search}</div>
-			</div>
-		);
-  	}
+    render() {
+        return (
+            <div className="container">
+                <h1>Wikipedia Viewer</h1>
+                <a className="random" href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
+                    Click here for a random article
+                </a>
+                <form onSubmit={this.getData}>
+                    <input type="text" id="search" name="search" className="form-control" placeholder="Search Wikipedia" autoComplete="off" />
+                </form>
+                <div className="items">{this.state.search}</div>
+            </div>
+        );
+    }
 }
 
 var app = document.getElementById("app");
