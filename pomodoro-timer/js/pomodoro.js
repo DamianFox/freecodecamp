daf4f1d233463b5d1c0daf2ce11876f2c23bc465
damiano.fossa <d.fossa.91@gmail.com>
2018-02-01 11:43:38 +0100

    Start timer click event

--- pomodoro-timer/js/pomodoro.js
+++ pomodoro-timer/js/pomodoro.js
@@ -2,27 +2,33 @@
 // Set the time in seconds we're counting down to
 var totalTime = 25*60;
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-    
-    // Time calculations for days, hours, minutes and seconds
-    var minutes = Math.floor(totalTime / 60);
-    var seconds = Math.floor(totalTime % 60);
+document.getElementById("start-btn").addEventListener("click", startTimer);
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-    // Output the result in an element with id="work-time"
-    document.getElementById("work-time").innerHTML = minutes + ":" + seconds;
+function startTimer(){
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        
+        // Time calculations for days, hours, minutes and seconds
+        var minutes = Math.floor(totalTime / 60);
+        var seconds = Math.floor(totalTime % 60);
 
-    totalTime--;
-    
-    // If the count down is over, write some text 
-    if (totalTime < 0) {
-        clearInterval(x);
-        document.getElementById("work-time").innerHTML = "EXPIRED";
-    }
-}, 1000);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        
+        // Output the result in an element with id="work-time"
+        document.getElementById("work-time").innerHTML = minutes + ":" + seconds;
+
+        totalTime--;
+
+        document.getElementById("message").innerHTML = "Started!!";
+        
+        // If the count down is over, write some text 
+        if (totalTime < 0) {
+            clearInterval(x);
+            document.getElementById("work-time").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
 
 
 
