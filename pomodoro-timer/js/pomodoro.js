9fd71d0a5af583284e686bb33fa638d709af6a37
damiano.fossa <d.fossa.91@gmail.com>
2018-02-01 21:40:27 +0100

    Button onclick event added

--- pomodoro-timer/js/pomodoro.js
+++ pomodoro-timer/js/pomodoro.js
@@ -1,12 +1,16 @@
 
 // Set the time in seconds we're counting down to
-var totalTime = 25*60;
+var totalTime = 10;
+var intervalID;
 
 document.getElementById("start-btn").addEventListener("click", startTimer);
+document.getElementById("stop-btn").addEventListener("click", stopTimer);
+document.getElementById("reset-btn").addEventListener("click", resetTimer);
 
 function startTimer(){
     // Update the count down every 1 second
-    var x = setInterval(function() {
+    intervalID = setInterval(function() {
+        console.log("Start");
         
         // Time calculations for days, hours, minutes and seconds
         var minutes = Math.floor(totalTime / 60);
@@ -20,14 +24,25 @@ function startTimer(){
 
         totalTime--;
 
-        document.getElementById("message").innerHTML = "Started!!";
+        document.getElementById("message").innerHTML = "Start";
         
         // If the count down is over, write some text 
         if (totalTime < 0) {
-            clearInterval(x);
-            document.getElementById("work-time").innerHTML = "EXPIRED";
+            clearInterval(intervalID);
         }
     }, 1000);
+}
+
+function stopTimer(){
+    clearInterval(intervalID);
+    document.getElementById("message").innerHTML = "Stop";
+}
+
+function resetTimer(){
+    clearInterval(intervalID);
+    totalTime = 10;
+    document.getElementById("work-time").innerHTML = "25:00";
+    document.getElementById("message").innerHTML = "Reset";
 }
 
 
