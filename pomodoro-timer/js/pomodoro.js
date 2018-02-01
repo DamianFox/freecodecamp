73ddbbac87d6bb7dc993d4587c8bf85624cff2c8
damiano.fossa <d.fossa.91@gmail.com>
2018-02-01 22:11:53 +0100

    Break time added

--- pomodoro-timer/js/pomodoro.js
+++ pomodoro-timer/js/pomodoro.js
@@ -1,16 +1,14 @@
 
-// Set the time in seconds we're counting down to
-var totalTime = 10;
 var intervalID;
 
-document.getElementById("start-btn").addEventListener("click", startTimer);
+document.getElementById("start-btn").addEventListener("click", workTimer);
 document.getElementById("stop-btn").addEventListener("click", stopTimer);
 document.getElementById("reset-btn").addEventListener("click", resetTimer);
 
-function startTimer(){
+function workTimer(){
     // Update the count down every 1 second
+    let totalTime = 10;
     intervalID = setInterval(function() {
-        console.log("Start");
         
         // Time calculations for days, hours, minutes and seconds
         var minutes = Math.floor(totalTime / 60);
@@ -24,11 +22,39 @@ function startTimer(){
 
         totalTime--;
 
-        document.getElementById("message").innerHTML = "Start";
+        document.getElementById("message").innerHTML = "Work";
         
         // If the count down is over, write some text 
         if (totalTime < 0) {
             clearInterval(intervalID);
+            breakTimer();
+        }
+    }, 1000);
+}
+
+function breakTimer(){
+    // Update the count down every 1 second
+    let totalTime = 5;
+    intervalID = setInterval(function() {
+        
+        // Time calculations for days, hours, minutes and seconds
+        var minutes = Math.floor(totalTime / 60);
+        var seconds = Math.floor(totalTime % 60);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        
+        // Output the result in an element with id="work-time"
+        document.getElementById("work-time").innerHTML = minutes + ":" + seconds;
+
+        totalTime--;
+
+        document.getElementById("message").innerHTML = "Break";
+        
+        // If the count down is over, write some text 
+        if (totalTime < 0) {
+            clearInterval(intervalID);
+            workTimer();
         }
     }, 1000);
 }
