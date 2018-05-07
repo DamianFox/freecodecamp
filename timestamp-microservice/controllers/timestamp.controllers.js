91a840c004bcc1b2cf95b5a3f5dd4b43c9d9eaea
damiano.fossa <d.fossa.91@gmail.com>
2018-05-07 23:29:12 +0200

    getTime function implemented

--- timestamp-microservice/controllers/timestamp.controllers.js
+++ timestamp-microservice/controllers/timestamp.controllers.js
@@ -1,5 +1,36 @@
+var moment = require('moment');
+
 // GET timestamp
 module.exports.getTime = function(req, res) {
 	var query = req.params.query;
 	console.log(query);
+	var unix = null;
+    var naturalDate = null;
+
+	if(+query >= 0){
+		unix = +query;
+		naturalDate = unixToNatural(unix);
+	}
+
+	if(isNaN(+query) && moment(query, "MMMM D, YYYY").isValid()){
+		unix = naturalToUnix(query);
+        naturalDate = unixToNatural(unix);
+	}
+
+	resObj = {
+		"unix": unix, 
+		"natural": naturalDate
+	};
+
+	res
+		.status(200)
+		.json(resObj);
 };
+
+function unixToNatural(num){
+	return moment.unix(num).format("MMMM DD, YYYY");
+}
+
+function naturalToUnix(date) {
+	return moment(date, "MMMM D, YYYY").format("X");
+}
