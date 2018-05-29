31380351bb9144fc43b3e2fe68aa23ee6d99c9f1
damiano.fossa <d.fossa.91@gmail.com>
2018-05-29 23:11:45 +0200

    preparing for modal dialog

--- recipe-box/src/app.js
+++ recipe-box/src/app.js
@@ -6,9 +6,21 @@ class Homepage extends Component {
 	constructor() {
         super();
         this.state = {
-            addRecipe: ''
+            addRecipe: '',
+            showModal: false
         }
+
         this.addRecipe = this.addRecipe.bind(this);
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+    }
+
+    showModal() {
+        this.setState({ showModal: true });
+    }
+
+    hideModal() {
+        this.setState({ showModal: false });
     }
 
     addRecipe(e) {
@@ -20,12 +32,33 @@ class Homepage extends Component {
             <div className="container">
             	<div className="row">
                 	<h1>Recipe Box</h1>
-                    <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#addRecipe">Add Recipe</button>
+                    <button type='button' onClick={this.showModal}>Add Recipe</button>
                 </div>
+                <Modal show={this.state.showModal} handleClose={this.hideModal} >
+                    <p>Modal</p>
+                    <p>Data</p>
+                </Modal>
             </div>
         );
     }
 }
+
+const Modal = ({ handleClose, show, children }) => {
+    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+
+    return (
+        <div className={showHideClassName}>
+                <section className='modal-main'>
+                    {children}
+                    <button
+                    onClick={handleClose}
+                    >
+                    Close
+                </button>
+            </section>
+        </div>
+    );
+};
 
 var app = document.getElementById("app");
 ReactDOM.render(<Homepage />, app);
