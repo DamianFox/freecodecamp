93234bd67ffa720fb816bf262e2c3d2783ab80f1
damiano.fossa <d.fossa.91@gmail.com>
2018-05-30 22:40:26 +0200

    Add input and textarea to modal

--- recipe-box/src/app.js
+++ recipe-box/src/app.js
@@ -32,7 +32,9 @@ class Homepage extends Component {
             <div className="container">
             	<div className="row">
                 	<h1>Recipe Box</h1>
-                    <button type='button' onClick={this.showModal}>Add Recipe</button>
+                </div>
+                <div className="row">
+                    <button className="btn btn-primary" type="button" onClick={this.showModal}>Add Recipe</button>
                 </div>
                 <Modal show={this.state.showModal} handleClose={this.hideModal} >
                     <p>Modal</p>
@@ -44,16 +46,27 @@ class Homepage extends Component {
 }
 
 const Modal = ({ handleClose, show, children }) => {
-    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
 
     return (
         <div className={showHideClassName}>
-                <section className='modal-main'>
-                    {children}
-                    <button
-                    onClick={handleClose}
-                    >
-                    Close
+            <section className="modal-main">
+                <div class="modal-header">
+                    <h5 class="modal-title text-black">Add Recipe</h5>
+                </div>
+                <div className="form-group">
+                    <label for="recipe-name">Recipe Name</label>
+                    <input type="text" className="form-control" id="recipe-name" placeholder="Enter name" />
+                </div>
+                <div className="form-group">
+                    <label for="ingredients">Ingredients</label>
+                    <textarea className="form-control" id="ingredients" rows="3" placeholder="Ingredients"></textarea>
+                </div>
+                {/*{children}*/}
+                <button className="btn btn-secondary"
+                onClick={handleClose}
+                >
+                Close
                 </button>
             </section>
         </div>
